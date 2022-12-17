f5c39245b89dae9b8f52eeae73fef2005ebbc8a6
JungKyungSong <lordsong5974@gmail.com>
2022-12-18 00:21:09 +0900

    start

--- components/Start.js
+++ components/Start.js
@@ -7,19 +7,50 @@ import {
   SafeAreaView,
   TouchableOpacity,
   Alert,
+  ImageBackground,
 } from 'react-native';
+import start from "../images/start.png"
 
-function Mypage({ navigation }) {
+function Start({ navigation }) {
     return (
-        <SafeAreaView>
-            <Text>START</Text>
-            <TouchableOpacity
-                onPress={() => navigation.navigate("Info")}
-            >
-            <Text>시작하기</Text>
-            </TouchableOpacity>
+        <SafeAreaView style={styles.container}>
+            <ImageBackground source={start} style={styles.image}>
+                <TouchableOpacity
+                    onPress={() => navigation.navigate("Info")}
+                    style={styles.start_btn}
+                >
+                <Text style={styles.start_text}>시작하기</Text>
+                </TouchableOpacity>
+            </ImageBackground>
         </SafeAreaView>
     );
 }
 
-export default Mypage;
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    image:{
+      width: "100%",
+      height: "100%",
+      resizeMode: "cover",
+    },
+    start_btn: {
+        backgroundColor: "#FFFFFF",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "86%",
+        height: "7%",
+        marginTop: "160%",
+        marginHorizontal: "7%",
+        borderRadius: 5,
+    },
+    start_text: {
+        color: "#2B82D4",
+        fontWeight: "bold"
+    }
+})
+
+export default Start;
