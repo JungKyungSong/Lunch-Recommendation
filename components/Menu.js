19a572ae2238baa3730c0e10d4c1fff53d3410b1
chanchanuu <ucp0650@naver.com>
2022-12-22 03:29:41 +0900

    edit

--- components/Menu.js
+++ components/Menu.js
@@ -8,7 +8,8 @@ import {
   TouchableOpacity,
   Alert,
   Image,
-  Button
+  Button,
+  ScrollView
 } from 'react-native';
 
 
@@ -20,65 +21,55 @@ const Menu = ({navigation}) => {
   const setclick3 = () => setselect(3);
   const setclick4 = () => setselect(4);
   
+  const cfArr = [
+    { id: "0", where: "횟집", image: '../images/carrot.png', bf: "설렁탕", lc: "오므라이스", dn: "김치찌개"},
+    { id: "1", where: "초밥", image: '../images/ex_images.png', bf: "수육국밥", lc: "치킨", dn: "피자"},
+    { id: "2", where: "돈부리", image: '../images/ex_images.png', bf: "순대국밥", lc: "부대찌개", dn: "뚝배기불고기"},
+    { id: "3", where: "라멘", image: '../images/ex_images.png', bf: "김밥", lc: "닭볶음탕", dn: "닭갈비"},
+    { id: "4", where: "돈까스", image: '../images/ex_images.png', bf: "감자탕", lc: "알밥", dn: "찜닭"},
+];
     return (
     
         <SafeAreaView style={styles.container}>
           <StatusBar style="auto"/>
-             <Text style={styles.title}>원하는 음식점 찾기</Text>           
-            <View style={styles.login_container}>
-                <Text style={styles.second_text}>지금 땡기는 메뉴를 선택해주세요.</Text>
-                
+            <Text style={styles.title}>원하는 음식점 찾기</Text>           
+            <ScrollView 
+                    style={styles.scroll_container}
+                    contentContainerStyle={{ flexGrow: 1 }}
+                    contentInsetAdjustmentBehavior="automatic">
+                    <View style={styles.cf_container}>
+                        {cfArr.map((array, index) => {
+                            return(
+                                <View
+                                        key={index}
+                                        style={styles.cf_each}
+                                >   
+                                    <View style={styles.image_container}>
+                                    <Image style={styles.image} source= {require('../images/ex_images.png')}/>
+                                    </View>
+                                    <View>
+                                        <Text style={styles.cf_text}>{array.where}</Text>
+                                    </View>
 
-                <View style={{height: 220, flexDirection: 'row'}}>
-                    <View style={{flex: 1, alignItems: "center"}}>
-                        <View style={{alignItems: 'center'}}>
-                        <Image source={require('../images/images.png')} style={{width: 150, height: 150}}/>
-                        </View>
-                        <TouchableOpacity>
-                        <Text style={styles.image_text}>한식</Text>
-                        </TouchableOpacity>
+                                </View>
+                            );
+                        })
+                        } 
                     </View>
-                    <View style={{flex: 1, alignItems: "center"}}>
-                        <View style={{alignItems: 'center'}}>
-                        <Image source={require('../images/images.png')} style={{width: 150, height: 150}}/>
-                        </View>
-                        <View>
-                        <Text style={styles.image_text}>양식</Text>
-                        </View>
-                    </View>
-                </View>
-                <View style={{height: 220, flexDirection: 'row'}}>
-                    <View style={{flex: 1, alignItems: "center"}}>
-                        <View style={{alignItems: 'center', }}>
-                        <Image source={require('../images/images.png')} style={{width: 150, height: 150}}/>
-                        </View>
-                        <View>
-                        <Text style={styles.image_text}>중식</Text>
-                        </View>
-                    </View>
-
-                    <View style={{flex: 1, alignItems: "center"}}>
-                        <View style={{alignItems: 'center', }}>
-                        <Image source={require('../images/images.png')} style={{width: 150, height: 150}}/>
-                        </View>
-                        <View>
-                        <Text style={styles.image_text}>일식</Text>
-                        </View>
-                    </View>
-                </View>
+                </ScrollView>
                 <TouchableOpacity
                         onPress={() => navigation.navigate("Result")}
                         style={styles.login_btn}
                     >
                     <Text style={styles.text}>다음</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
+              </TouchableOpacity>
+              <TouchableOpacity
                         onPress={() => navigation.navigate("Restaurant")}
                         style={styles.register_btn}
                     >
                     <Text style={styles.text}>이전</Text>
-            </TouchableOpacity>
-            </View>
+              </TouchableOpacity>
+            
 
         </SafeAreaView>
     );
@@ -87,129 +78,111 @@ const Menu = ({navigation}) => {
 
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    image:{
-      width: "100%",
-      height: "100%",
-      resizeMode: "cover",
-    },
-    title: {
-      fontSize: 25,
-      fontWeight: "bold",
-      color: "#2B82D4",
-      textAlignVertical: 'center',
-      textAlign: 'center',
-      marginTop: "10%"
-    },
-    login_container: {
-      backgroundColor: "#FFFFFF",
-      alignItems: "center",
-      justifyContent: "center",
-      width: "90%",
-      height: "95%",
-      marginTop: "8%",
-      marginHorizontal: "8%",
-      borderRadius: 10,
-      shadowColor: "gray",
-      shadowOffset: {
-        width: 0,
-        height: 2,
-      },
-      shadowOpacity: 0.25,
-      shadowRadius: 3.84,
-    },
-    container_title: {
+  container: {
+    flex: 1,
+    alignItems: "center",
+    //justifyContent: "center",
+  },
+  bg_image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+  },
+  image:{
+      width: 80,
+      height: 80,
+      marginLeft: "20%",
+      marginRight: "-10%"
+  },
+  title_container: {
+      flexDirection: "row",
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: "bold",
+    color: "#2B82D4",
+    textAlignVertical: 'center',
+    textAlign: 'center',
+    marginTop: "10%"
+  },
+  subtitle: {
       fontSize: 20,
-      fontWeight: "bold",
-      color: "#878787",
-      marginBottom: "4%"
-    },
-    login_btn: {
-        backgroundColor: "#2B82D4",
-        alignItems: "center",
-        justifyContent: "center",
-        width: "80%",
-        height: "5%",
-        marginHorizontal: "10%",
-        borderRadius: 5,
-        marginTop: "5%"
-      },
-      title_btn: {
-        backgroundColor: "#2B82D4",
-        alignItems: "center",
-        justifyContent: "center",
-        width: "95%",
-        height: "5%",
-        marginHorizontal: "40%",
-        borderRadius: 5,
-        marginTop: "-16%"
-      },
-      register_btn: {
-        backgroundColor: "#FFFFFF",
-        alignItems: "center",
-        justifyContent: "center",
-        width: "80%",
-        height: "5%",
-        marginTop: "5%",
-        marginHorizontal: "10%",
-        borderColor: "#92BEE7",
-        borderWidth: 1,
-        borderRadius: 5,
-      },
-      login_text: {
-        color: "#FFFFFF",
-      },
-      register_text: {
-        color: "#2B82D4",
-      }, 
-      input: {
-        marginTop: "1%",
-        paddingHorizontal: 10,
-        width: 260,
-        height: 40,
-        borderBottomColor: '#2B82D4',
-        borderBottomWidth: 1,
-    },
-    text: {
-        fontSize:22,
-        marginTop: "1%"
-    },
-    second_text: {
-        fontSize:18,
-        marginTop: "-25%"
-    },
-    image_text: {
-        fontSize:22,
-        marginTop: "10%"
-    },
-    user:{
-        backgroundColor:'white',
-        marginHorizontal:20,
-    }
+      color: "#E6E6E6",
+      marginTop: "160%",
+      marginLeft: "4%"
+  },
+  scroll_container: {
+      marginTop: "10%",
+      width: "100%",
+  },
+  cf_container:{
+      height: 1000
+  },
+  cf_each:{
+      //backgroundColor: "blue",
+      width: "100%",
+      height: "20%",
+      flexDirection: "row",
+      borderBottomColor: '#EFEFEF',
+      borderBottomWidth: 2,
+      paddingTop: "3%"
+  },
+  cf_text:{
+      textAlign: "left",
+      fontSize: 26,
+      paddingTop: "8%",
+      marginTop: "18%",
+      marginLeft: "5%"
+  },
+  menu_container: {
+      marginTop: "22%",
+      marginLeft: "-20%"
+  },
+  bf_container: {
+      flexDirection: "row",
+      marginTop: "12%"
+  },
+  lc_container: {
+      flexDirection: "row",
+      marginTop: "12%"
+  },
+  dn_container: {
+      flexDirection: "row",
+      marginTop: "12%"
+  },
+  cf_menu: {
+      fontSize: 18,
+      fontWeight: "bold"
+  },
+  cf_menu_detail: {
+      color: "#939393"
+  },
+  login_btn: {
+    backgroundColor: "#2B82D4",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "80%",
+    height: "5%",
+    marginHorizontal: "10%",
+    borderRadius: 5,
+    marginTop: "5%"
+  },
+  register_btn: {
+    backgroundColor: "#FFFFFF",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "80%",
+    height: "5%",
+    marginTop: "5%",
+    marginHorizontal: "10%",
+    borderColor: "#92BEE7",
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  text: {
+    fontSize:22,
+    marginTop: "1%"
+}
 })
 
-/*const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        paddingTop:StatusBar.currentHeight,
-        backgroundColor: '#FFFFFF',
-    },
-    scrollView:{
-        backgroundColor:'white',
-        marginHorizontal:20,
-    },
-    user:{
-        backgroundColor:'white',
-        marginHorizontal:20,
-    },
-    text: {
-        fontSize:22, alignItems: "center"
-    },
-});*/
-
 export default Menu;
-
