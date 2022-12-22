2ba97def324c709f9c411980a5c80d441a38bf5b
JungKyungSong <lordsong5974@gmail.com>
2022-12-23 05:51:54 +0900

    g

--- components/Mypage.js
+++ components/Mypage.js
@@ -460,7 +460,7 @@ const sendResult = async () => {
         </View>
         </View>
         <TouchableOpacity
-                    onPress={() => setOk(!true)}
+                    onPress={() => setOk(!ok)}
                     style={styles.login_btn}
                 >
                 <Text style={styles.text}>다음</Text>
